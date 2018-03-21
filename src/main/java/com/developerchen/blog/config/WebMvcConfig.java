d1cc0dc8cd15697f1ce4b02a5d9e0d025bdc8cd2
syuchen <sunyuchen1990@gmail.com>
2018-03-21 15:20:35 +0800

    添加web部分的配置文件
    添加web部分的配置文件WebMvcConfig

---
+++ src/main/java/com/developerchen/blog/config/WebMvcConfig.java
@@ -0,0 +1,9 @@
+package com.developerchen.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+}
