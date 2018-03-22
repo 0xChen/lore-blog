9e7f5914dc395b52d9b9b696dae75273f868f6e9
syuchen <sunyuchen1990@gmail.com>
2018-03-22 08:52:28 +0800

    rename SecurityConfig -> WebSecurityConfig

--- src/main/java/com/developerchen/blog/config/WebSecurityConfig.java
+++ src/main/java/com/developerchen/blog/config/WebSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     // @formatter:off
