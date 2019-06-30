d1cb57a66e9de46219e7d3451c24005f026fb4cf
晨 <sunyuchen1990@gmail.com>
2019-06-30 19:43:48 +0800

    Update LogServiceImpl.java
    @Async 注解标准的方法被调用执行时, 先是以org.springframework.core.task.TaskExecutor类型去寻找执行器执行.因为同时启用了@EnableAsync
    @EnableScheduling两个注解, 所以会有两个实现了TaskExecutor接口类型的bean:ThreadPoolTaskExecutor和ThreadPoolTaskScheduler!log为debug级别时会看到提示信息. 所以这里直接指定以bean名称获取目标bean.

--- src/main/java/com/developerchen/core/service/impl/LogServiceImpl.java
+++ src/main/java/com/developerchen/core/service/impl/LogServiceImpl.java
@@ -3,6 +3,7 @@ package com.developerchen.core.service.impl;
 import com.developerchen.core.domain.entity.Log;
 import com.developerchen.core.repository.LogMapper;
 import com.developerchen.core.service.ILogService;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -25,7 +26,7 @@ public class LogServiceImpl extends BaseServiceImpl<LogMapper, Log> implements I
         baseMapper.insert(log);
     }
 
-    @Async
+    @Async(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
     @Override
     public void asyncSaveLog(Log log) {
         baseMapper.insert(log);
