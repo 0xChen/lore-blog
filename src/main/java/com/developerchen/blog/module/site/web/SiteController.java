587fd70272d2c10c98c0ee2b9d781909836fa4ae
晨 <sunyuchen1990@gmail.com>
2019-08-23 15:42:30 +0800

    对页面传回的数据加入验证

--- src/main/java/com/developerchen/blog/module/site/web/SiteController.java
+++ src/main/java/com/developerchen/blog/module/site/web/SiteController.java
@@ -8,6 +8,7 @@ import com.developerchen.core.domain.entity.User;
 import com.developerchen.core.web.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -70,7 +71,7 @@ public class SiteController extends BaseController {
      */
     @ResponseBody
     @PostMapping(BlogConst.INSTALL_URI + "/user")
-    public RestResponse installAdminUser(User user) {
+    public RestResponse installAdminUser(@Validated User user) {
         try {
             this.siteService.installAdminUser(user);
         } catch (Exception e) {
